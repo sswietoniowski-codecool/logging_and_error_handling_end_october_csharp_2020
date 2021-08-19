a79205b772c0bec283807d06ea118ffeabc782d8
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-19 21:45:04 +0200

    Used different logging levels and events

---
+++ Data/DataEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace logging.Data
+{
+    public class DataEvents
+    {
+        public static EventId GetAllProducts = new EventId(10001, "GetAllProducts");
+    }
+}
--- Data/ProductRepository.cs
+++ Data/ProductRepository.cs
@@ -8,6 +8,14 @@ using Microsoft.Extensions.Logging;
 
 namespace logging.Data
 {
+    public static class RandomExtension
+    {
+        public static bool NextBool(this Random random, int truePercentage = 50)
+        {
+            return random.NextDouble() < (truePercentage / 100.0);
+        }
+    }
+
     public class ProductRepository : IProductRepository
     {
         private ILogger<ProductRepository> _logger;
@@ -21,16 +29,27 @@ namespace logging.Data
         {
             _logger.LogDebug($"{nameof(GetAllProducts)}: Retrieving all products...");
 
-            var products = new List<Product>()
+            Random random = new Random();
+
+            if (random.NextBool())
             {
-                new Product {Id = 1, Name = "Product A", Price = 10},
-                new Product {Id = 2, Name = "Product B", Price = 20},
-                new Product {Id = 3, Name = "Product C", Price = 30}
-            };
+                var products = new List<Product>()
+                {
+                    new Product {Id = 1, Name = "Product A", Price = 10},
+                    new Product {Id = 2, Name = "Product B", Price = 20},
+                    new Product {Id = 3, Name = "Product C", Price = 30}
+                };
 
-            _logger.LogDebug($"{nameof(GetAllProducts)}: Retrieved all products");
+                _logger.LogDebug($"{nameof(GetAllProducts)}: Retrieved all products");
 
-            return products;
+                return products;
+            }
+            else
+            {
+                _logger.LogError(DataEvents.GetAllProducts, $"{nameof(GetAllProducts)}: Unable to retrieve all products");
+
+                return new List<Product>();
+            }
         }
     }
 }
